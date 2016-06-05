5d0c7bd612ae2c4db022617f2589f8540fb5acf8
pijoules <lchan1994@yahoo.com>
2016-06-05 13:55:03 -0400

    gotta increase that github score

--- VoiceStream.java
+++ VoiceStream.java
@@ -2,16 +2,29 @@ import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
 
 public class VoiceStream {
-    private static VoiceManager voiceManager = VoiceManager.getInstance();
-    private static Voice voice = voiceManager.getVoice("kevin16");
+    private final static VoiceManager voiceManager = VoiceManager.getInstance();
+    private final static Voice voice = voiceManager.getVoice("kevin16");
+    private static boolean allocated = false;
 
     public static void print(String s){
+        if (!allocated){
+            voice.allocate();
+            allocated = true;
+        }
         voice.speak(s);
     }
     public static void println(String s){
+        if (!allocated){
+            voice.allocate();
+            allocated = true;
+        }
         voice.speak(s);
     }
     public static void printf(String s, Object... objs){
+        if (!allocated){
+            voice.allocate();
+            allocated = true;
+        }
         voice.speak(String.format(s, objs));
     }
 }
